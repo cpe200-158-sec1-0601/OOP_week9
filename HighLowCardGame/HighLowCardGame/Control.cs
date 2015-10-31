bb45a50ab4b2bce5f5fa92711e8be13b8d87c5be
Watchanan Chanthaphakun <tmwatchanan@gmail.com>
2015-10-31 23:39:20 +0700

    High-Low Card Game console application v.4 (working)
    !! Control.cs
    + Added a shuffle condition, 2 player's cards left, that only shuffle for only one player's deck in TieTurn method.
    ! Modified and changed some comparison checking in CompareCardDeck method.
    ! Changed counting rank of all cards by Ace is count as highest rank.

--- HighLowCardGame/HighLowCardGame/Control.cs
+++ HighLowCardGame/HighLowCardGame/Control.cs
@@ -48,7 +48,10 @@ namespace HighLowCardGame
         {
             Console.WriteLine("[Tie] Reshuffle the both players card deck");
             pPlayer1.PlayingDeck.Shuffle();
-            pPlayer2.PlayingDeck.Shuffle();
+            if (pPlayer1.PlayingDeck.Cards.Count != 2)
+            {
+                pPlayer2.PlayingDeck.Shuffle();
+            }
         }
 
         public static void RemovePlayerCards(Player pPlayer1, Player pPlayer2, int range)
@@ -61,7 +64,7 @@ namespace HighLowCardGame
 
         public static int CompareCardDeck(Player pPlayer1, Player pPlayer2)
         {
-            if (pPlayer1.PlayingDeck.Cards.Count == 0)
+            if (pPlayer1.PlayingDeck.Cards.Count == 0) // No longer be playing
             {
                 Console.WriteLine("[Summary] No more card left in the both players card deck");
                 return -1;
@@ -71,15 +74,38 @@ namespace HighLowCardGame
             int pPlayer2_last = pPlayer2.PlayingDeck.Cards[LastCard].Value;
             Console.WriteLine("Player 1 has " + pPlayer1.PlayingDeck.Cards[LastCard]);
             Console.WriteLine("Player 2 has " + pPlayer2.PlayingDeck.Cards[LastCard]);
+            if (pPlayer1.PlayingDeck.Cards.Count == 1 && pPlayer1.PlayingDeck.Cards[LastCard].Value == pPlayer2.PlayingDeck.Cards[LastCard].Value) // No longer be playing
+            {
+                Console.WriteLine("[Tie] The last card of both players is the same.");
+                return -1;
+            }
             if (pPlayer1_last == pPlayer2_last)
             {
-                if (pPlayer1.PlayingDeck.Cards.Count == 1)
+                bool Continue_Game = true;
+                for (int i = 0; i <= LastCard; i++)
                 {
-                    Console.WriteLine("[Tie] The last card of both players is the same.");
+                    for (int j = 0; j <= LastCard; j++)
+                    {
+                        if (pPlayer1.PlayingDeck.Cards[i].Value > pPlayer2.PlayingDeck.Cards[j].Value)
+                        {
+                            Continue_Game = false;
+                        }
+                        else
+                        {
+                            Continue_Game = true;
+                        }
+                    }
+                }
+                if (!Continue_Game)
+                {
+                    Console.WriteLine("== [Player 1] Card deck is containing these cards :");
+                    pPlayer1.PlayingDeck.ViewCardsinDeck();
+                    Console.WriteLine("== [Player 2] Card deck is containing these cards :");
+                    pPlayer2.PlayingDeck.ViewCardsinDeck();
                     return -1;
                 }
                 int NumberFromLastCard = pPlayer1_last;
-                if (NumberFromLastCard > LastCard)
+                if (NumberFromLastCard > LastCard) // More value of the card than number of left cards
                 {
                     TieTurn(pPlayer1, pPlayer2);
                     Console.WriteLine("== [Player 1] Card deck is containing these cards :");
@@ -93,33 +119,33 @@ namespace HighLowCardGame
                 Console.WriteLine("Player 2 has " + pPlayer2.PlayingDeck.Cards[NumberFromLastCard]);
                 int pPlayer1_fromlast = pPlayer1.PlayingDeck.Cards[NumberFromLastCard].Value;
                 int pPlayer2_fromlast = pPlayer2.PlayingDeck.Cards[NumberFromLastCard].Value;
-                if (pPlayer1_fromlast > pPlayer2_fromlast)
+                if (pPlayer1_fromlast < pPlayer2_fromlast) // Player 1 WIN
                 {
                     PlayerWinTurn(pPlayer1, NumberFromLastCard);
                     RemovePlayerCards(pPlayer1, pPlayer2, NumberFromLastCard);
                     return 1;
                 }
-                else if (pPlayer1_fromlast < pPlayer2_fromlast)
+                else if (pPlayer1_fromlast > pPlayer2_fromlast) // Player 2 WIN
                 {
                     PlayerWinTurn(pPlayer2, NumberFromLastCard);
                     RemovePlayerCards(pPlayer1, pPlayer2, NumberFromLastCard);
                     return 2;
                 }
-                else
+                else // Tie
                 {
                     TieTurn(pPlayer1, pPlayer2);
                     return 0;
                 }
             }
             // Player 1 WIN
-            else if (pPlayer1_last > pPlayer2_last)
+            else if (pPlayer1_last < pPlayer2_last)
             {
                 PlayerWinTurn(pPlayer1);
                 RemovePlayerCards(pPlayer1, pPlayer2, 1);
                 return 1;
             }
             // Player 2 WIN
-            else if (pPlayer1_last < pPlayer2_last)
+            else if (pPlayer1_last > pPlayer2_last)
             {
                 PlayerWinTurn(pPlayer2);
                 RemovePlayerCards(pPlayer1, pPlayer2, 1);
